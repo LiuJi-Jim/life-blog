84e89d2914b57c4ce95d007250541d9c140720a6
LiuJi-Jim <jimnox0491@gmail.com>
2015-10-15 17:54:13 +0800

    Site updated: 2015-10-15 17:54:11

--- uploads/public/rename.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs')
-
-fs.readdirSync('./').filter(name => name.endsWith('.png'))
-    .forEach(name => {
-        const number = name.match(/\d+/)[0]
-        fs.renameSync(name, `AC-${number}.png`)
-    })
--- uploads/public/rename.sh
+++
@@ -1,6 +0,0 @@
-for file in ls *.png
-do
-    number=`echo $file | egrep -o "\d+"`
-    mv \"$file\" \"AC-${number}.png\"
-done
-
